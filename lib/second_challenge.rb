137fb02962ced6cf236eb04e58a30ecdcabcc534
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-01 14:45:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {name: "Lord Montague"},
-      :matriarch => {name: "Lady Montague"},
-      :hero => {name: "Romeo"},
-      :hero_friends => [names: "Benvolio", "Mercucio"]
+      :patriarch => {},
+      :matriacrch => {},
+      :hero => {},
+      :hero_friends => []
     }
     :capulet => {
-      :patriarch => {name: "Lord Capulet"},
-      :matriarch => {name: "Lady Capulet"},
-      :heroine => {name: "Juliet"},
-      :heroine_friends => [names: "Steven", "Nurse"]
+      :patriarch => {},
+      :matriacrch => {},
+      :heroine => {},
+      :heroine_friends => []
     }
   }
 end
